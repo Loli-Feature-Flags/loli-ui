367f557711f8e0436e96dc17de02af1f80e3a0dc
Peter Kuhmann <info@peter-kuhmann.de>
2024-07-11 22:21:18 +0200

    Dark mode: views lists + list entries + data type badges

--- src/components/shared/FeatureFlagTypeBadge.vue
+++ src/components/shared/FeatureFlagTypeBadge.vue
@@ -1,20 +1,12 @@
 <script setup lang="ts">
 import type { FeatureFlagType } from '@loli-feature-flags/loli-sdk'
+import DataTypeBadge from './DataTypeBadge.vue'
 
 defineProps<{ type: FeatureFlagType }>()
 </script>
 
 <template>
-  <span
-    :class="[
-      'border rounded-md px-2 py-0.5 text-xs',
-      {
-        'border-primary-500 bg-primary-100 text-primary-700': type === 'boolean',
-        'border-cerulean-500 bg-cerulean-100 text-cerulean-800': type === 'string',
-        'border-zest-500 bg-zest-100 text-zest-700': type === 'number'
-      }
-    ]"
-  >
+  <DataTypeBadge :type="type">
     {{ $t(`spec.featureFlag.type.${type}`) }}
-  </span>
+  </DataTypeBadge>
 </template>
--- src/components/shared/ListEntryCard.vue
+++ src/components/shared/ListEntryCard.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import type { IssuesCarryOn } from '../../utils/issues'
+import { countPropertyReferences, isPropertyReferenced } from '@loli-feature-flags/loli-sdk'
 
 defineProps<{ issuesCarryOn?: IssuesCarryOn }>()
 defineEmits(['click'])
@@ -14,13 +15,26 @@ defineEmits(['click'])
     :class="[
       'transition focus-ring cursor-pointer rounded-md p-4 text-left border',
       {
-        'border-gray-200 bg-gray-50/50 hover:bg-gray-50 active:bg-gray-100':
+        'border-gray-200 bg-gray-50/50 hover:bg-gray-50 active:bg-gray-100 dark:bg-gray-800/50 dark:border-gray-700 dark:hover:bg-gray-800/80 dark:active:bg-gray-800/20':
           !issuesCarryOn || issuesCarryOn.isValid(),
-        'border-red-600 bg-red-50/30 hover:bg-red-50/80 active:bg-red-100':
-          issuesCarryOn && issuesCarryOn.isInvalid()
+        'border-red-600 bg-red-50/30 hover:bg-red-50/80': issuesCarryOn && issuesCarryOn.isInvalid()
       }
     ]"
   >
-    <slot />
+    <div class="flex flex-row items-center justify-between flex-wrap gap-4">
+      <span
+        class="text-lg font-mono flex flex-row items-center gap-4 text-primary-700 dark:text-gray-300"
+      >
+        <slot name="title-start" />
+      </span>
+
+      <div class="flex flex-row gap-4 items-center">
+        <slot name="title-end" />
+      </div>
+    </div>
+
+    <div class="flex flex-col gap-0.5 items-start text-xs text-gray-400 mt-2" v-if="$slots.bottom">
+      <slot name="bottom" />
+    </div>
   </div>
 </template>
